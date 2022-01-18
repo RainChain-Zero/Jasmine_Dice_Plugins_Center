01aff41a7fc038ef8c4ed427652c804a68cd955f
B20031725钱文涛 <3032902237@qq.com>
2022-01-18 10:45:35 +0800

    add api for pictures

--- additional.lua
+++ additional.lua
@@ -10,3 +10,18 @@ function test(msg)
     end
 end
 msg_order[Trade_order]="test"
+
+function picture(msg)
+    return "[CQ:image,url="..picture_api[ranint(1,#picture_api)].."]"
+end
+msg_order["来点二次元"]="picture"
+
+picture_api=
+{
+    [1]="https://iw233.cn/api/Random.php",
+    [2]="http://random.firefliestudio.com",
+    [3]="https://acg.toubiec.cn/random.php",
+    [4]="https://www.dmoe.cc/random.php",
+    [5]="https://api.ixiaowai.cn/api/api.php",
+}
+
