f448409175134b0b88a3fb56d28cbdac24c11c63
B20031725钱文涛 <3032902237@qq.com>
2022-01-30 21:13:58 +0800

    update to beta version

--- 好感互动系统/TradeSystem.lua
+++ 好感互动系统/TradeSystem.lua
@@ -1,12 +1,20 @@
 --[[
     @author 慕北_Innocent(RainChain)
-    @version 1.2(Alpha)
+    @version 1.3(Beta)
     @Create 2021/11/21 0:21
-    @Last Update 2022/01/21 22:01
+    @Last Update 2022/01/30 21:09
     ]]
 
 msg_order={}
 
+package.path=getDiceDir().."/plugin/ReplyAndDescription/?.lua"
+require "itemDescription"
+
+itemRequest=""
+itemReceive=""
+QQReceive=""
+itemReceiveNum=""
+itemRequestNum=""
 --交易系统
 --//! 注意！同一用户只能有一次进行的交易，如果多次进行，只保留最后一次请求，之前的请求视作无效
 trade_order="交易"
@@ -20,15 +28,26 @@ function Trade(msg)
             isInGroup=1
         else
             isInGroup=0
+            --! 禁止跨群交易（防止主动发起临时对话）
+            return "系统：检测到您的参数输入有误哦~"
         end
         --正则检测并选出参数
         --//todo 注意输入合法性的判断条件
-        local QQReceive,itemRequestNum,itemRequest,itemReceiveNum,itemReceive=string.match(msg.fromMsg,"^[%s]*[%[CQ:at,qq=]*(%d*)[%]]*[%s]*[T,t]*[%s]*(%d*)[%s]*(%S*)[%s]*[F,f]*[%s]*(%d*)[%s]*(%S*)$",#trade_order+1)
+        QQReceive,itemRequestNum,itemRequest,itemReceiveNum,itemReceive=string.match(msg.fromMsg,"^[%s]*[%[CQ:at,qq=]*(%d*)[%]]*[%s]*[T,t]*[%s]*(%d*)[%s]*(%S*)[%s]*[F,f]*[%s]*(%d*)[%s]*(%S*)$",#trade_order+1)
         if((QQReceive==nil or QQReceive=="")  or (itemRequestNum==nil or itemRequestNum=="") or (itemRequest==nil or itemRequest=="") or (itemReceiveNum==nil or itemReceiveNum=="") or string.find(msg.fromMsg,"@")~=nil)then
             return "系统：检测到您的参数输入有误哦~"
         end
         if(msg.fromQQ==QQReceive)then
             return "系统：您无法和自己交易哦~"
+        end
+        --? 输入的道具是否存在
+        local  flag1,flag2=Check()
+        if(not flag1 or not flag2)then
+            return "系统：检测到您输入了数据库中不存在的道具呢"
+        end
+        --! 禁止好感交易
+        if(string.find(itemRequest,"好感")~=nil or string.find(itemReceive,"好感")~=nil)then
+            return "重要数据不能进行交易哦？~"
         end
         local itemRequestNow=getUserConf(msg.fromQQ,itemRequest,0)
         if(itemRequestNow-itemRequestNum*1<0)then
@@ -216,13 +235,33 @@ function adminGift(msg)
     end
     --目标 赠送道具数量 赠送道具 附加信息
     local QQ,num,item,message=string.match(msg.fromMsg,"[%s]*(%d*)[%s]*(%d*)[%s]*(%S*)[%s]*(.*)",#admin_order_gift+1)
-    if(QQ==nil or num==nil or item==nil or message==nil)then
+    if(QQ==nil or QQ=="" or num==nil or item==nil or message==nil)then
         return "参数输入有误！"
     end
     setUserConf(QQ,item,getUserConf(QQ*1,item,0)+num)
     --发送消息提醒对方
+    --! 注意 一定要在对方是好友的前提下使用！
     local content="系统邮件："..message.."\n已接收附件："..item.."x"..string.format("%.0f",num)..",通过指令“查询 道具名”确认"
     sendMsg(content,0,QQ)
     return "权限确认：已成功将奖励送至目标"
 end
 msg_order[admin_order_gift]="adminGift"
+
+--判断输入的道具是否合法
+function Check()
+    local flag1,flag2=true,true
+    for k,_ in pairs(Item)do
+        if(string.find(k,itemRequest)~=nil)then
+            flag1=true
+            itemRequest=k
+        end
+        if(string.find(k,itemReceive)~=nil)then
+            flag2=true
+            itemReceive=k
+        end
+        if (flag1 and flag2) then
+            return true,true
+        end
+    end
+    return flag1,flag2
+end
