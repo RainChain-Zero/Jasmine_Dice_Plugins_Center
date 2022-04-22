6a2c66cce6ac3649dcb548c8a1e959fa39ed1f90
RainChain-Zero <rainchain@foxmail.com>
2022-04-22 12:58:45 +0800

    enhance picture

--- additional.lua
+++ additional.lua
@@ -1,10 +1,8 @@
 msg_order = {}
 Trade_order = "禁言"
 function Mute(msg)
-    if (msg.fromQQ == "3032902237" or msg.fromQQ=="2677409596") then
-        local QQ, time = string.match(msg.fromMsg,
-                                      "[%s]*[%[CQ:at,qq=]*(%d*)[%]]*[%s]*(%d*)",
-                                      #Trade_order + 1)
+    if (msg.fromQQ == "3032902237" or msg.fromQQ == "2677409596") then
+        local QQ, time = string.match(msg.fromMsg, "[%s]*[%[CQ:at,qq=]*(%d*)[%]]*[%s]*(%d*)", #Trade_order + 1)
         if (QQ == nil or QQ == "") then
             return "{nick} 请告诉茉莉目标是哪位小朋友哦~"
         end
@@ -14,7 +12,23 @@ end
 msg_order[Trade_order] = "Mute"
 
 function picture(msg)
-    return "[CQ:image,url=" .. picture_api[ranint(1, #picture_api)] .. "]"
+    local num = string.match(msg.fromMsg, "%d+")
+    if (num == nil or num == "") then
+        num = 1
+    end
+    num = num * 1
+    if (num > 6) then
+        return "『✖参数超限』一次不能超过6张哦~"
+    end
+    local reply = ""
+    while (num > 0) do
+        reply = reply .. "[CQ:image,url=" .. picture_api[ranint(1, #picture_api)] .. "]"
+        if (num > 1) then
+            reply = reply .. "\f"
+        end
+        num = num - 1
+    end
+    return reply
 end
 msg_order["来点二次元"] = "picture"
 
