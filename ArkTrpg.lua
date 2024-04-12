d0645472420a5c7db3443c0d60d0ad5b31f6bc9a
RainChain-Zero <3032902237@qq.com>
2024-04-12 19:44:18 +0800

    feat: ArkTrpg 2.1

--- ArkTrpg.lua
+++ ArkTrpg.lua
@@ -1,14 +1,22 @@
 --[[
     @Author RainChain-Zero
-    @Version 2.0
-    @Last Modified 2023/11/6 18:19
+    @Version 2.1
+    @Last Modified 2024/02/24
     @Description：适用于方舟泰拉trpg的规则检定
 ]]
 ---@diagnostic disable: lowercase-global, assign-type-mismatch, cast-local-type
 msg_order = {}
-at_cq = "%[CQ:at,id=(%d+)%]"
 order_kng = ".rk"
 msg_order[order_kng] = "ark_main"
+
+at_cq = "%[CQ:at,id=(%d+)%]"
+BASIC_ATTR = {
+   ["欺诈，乔装，潜行，调查，觉察，追踪"] = "精神意志",
+   ["声乐，艺术，心理，游说，取悦，威吓"] = "个人魅力",
+   ["妙手，急救，驾驶，杂耍，短兵，暗器，弓弩，身法"] = "反应机动",
+   ["长兵，刀剑，钝器，格斗，软兵，拳术，盾术"] = "物理强度",
+   ["兵械操作，生物驯养，军事理论，领导力，自然学，工程学，医药学，源石学，社会学，政法学，经管学，神秘学，机械工程，电子工程，农林牧渔，加工工艺"] = "经验智慧"
+}
 
 objTemp, obj, num, sign, add = "", "", "", "", ""
 
@@ -17,8 +25,10 @@ function ark_main(msg)
    local str = msg.fromMsg:gsub("^。", ".")
    -- 判断是.rk还是.rka
    local isrka, at_qq = false, nil
-   -- 判断是否.rkc和可能的成功阈值
-   local isrkc, succ_threshold = false, nil
+   -- 判断是否.rkc和可能的dc
+   local isrkc, dc = false, nil
+   -- 判断是否是rkd和骰面
+   local isrkd, dice = false, nil
    if msg.fromMsg:match("^.rka") then
       isrka = true
       -- 获取rka的at_qq
@@ -28,15 +38,24 @@ function ark_main(msg)
    end
    if msg.fromMsg:match("^.rkc") then
       isrkc = true
-      -- 获取rkc的成功阈值
-      succ_threshold = str:match("/%s*(%d+)")
+      -- 获取rkc的dc
+      dc = str:match("/%s*(%d+)")
       --去掉"c"
       str = str:gsub("^.rkc", ".rk")
    end
+   if msg.fromMsg:match("^.rkd") then
+      isrkd = true
+      -- 获取rkd的骰面和dc(dice/dc)
+      dice, dc = str:match("(%d+)%s*/%s*(%d+)")
+      -- 获取可能的at_qq
+      at_qq = str:match(at_cq)
+      --去掉"d"
+      str = str:gsub("^.rkd", ".rk")
+   end
    -- 去掉可能的at_qq
    str = str:gsub(at_cq, "")
-   -- 去掉可能的成功阈值
-   str = str:gsub("/%s*(%d+)", "")
+   -- 去掉可能的dice和dc
+   str = str:gsub("%d*%s*/%s*%d+", "")
 
    local res, round = "", 1
    str, round = ark_multi(str)
@@ -59,7 +78,7 @@ function ark_main(msg)
          if j ~= 1 then
             union_item = ".rk " .. union_item
          end
-         local res_now = ark_check(msg, union_item, isrka, at_qq, isrkc, succ_threshold)
+         local res_now = ark_check(msg, union_item, isrka, at_qq, isrkc, dc, isrkd, dice)
          -- 修改多次检定的剩余结果抬头
          if not isunion and i ~= 1 then
             res_now = res_now:gsub("{pc}进行ark的(.*)检定:", "")
@@ -151,7 +170,7 @@ function cal_mod(mod)
    return mod_now
 end
 
-function ark_check(msg, str, isrka, at_qq, isrkc, succ_threshold)
+function ark_check(msg, str, isrka, at_qq, isrkc, dc, isrkd, dice)
    -- rka ac
    local ac, res_rka = 0, ""
    if isrka and at_qq then
@@ -188,7 +207,7 @@ function ark_check(msg, str, isrka, at_qq, isrkc, succ_threshold)
    if ((type(num) == "string" and (num == "" or num == nil)) and not sign_judge) then
       --存在即时检定但无修订值
       --! 已知问题：如果检定条目为“力量”，将会取出nil
-      num = getPlayerCardAttr(msg.fromQQ, msg.gid, obj, 0)
+      num = getPlayerCardAttr(msg.uid, msg.gid, obj, 0)
 
       if (num == nil) then
          return ""
@@ -203,12 +222,12 @@ function ark_check(msg, str, isrka, at_qq, isrkc, succ_threshold)
    elseif (type(num) == "string" and (num == "" or num == nil) and sign_judge) then
       --既有即时检定又有修订值
       if (sign == "+") then
-         num = getPlayerCardAttr(msg.fromQQ, msg.gid, obj, 0) - ac + add * 1
+         num = getPlayerCardAttr(msg.uid, msg.gid, obj, 0) - ac + add * 1
          if (num <= 0) then
             return "成功率<=0，这样可成功不了哦？"
          end
       elseif (sign == "-") then
-         num = getPlayerCardAttr(msg.fromQQ, msg.gid, obj, 0) - ac - add * 1
+         num = getPlayerCardAttr(msg.uid, msg.gid, obj, 0) - ac - add * 1
          if (num <= 0) then
             return "成功率<=0，这样可成功不了哦？"
          end
@@ -234,7 +253,11 @@ function ark_check(msg, str, isrka, at_qq, isrkc, succ_threshold)
    end
    -- 如果是rkc检定
    if isrkc then
-      return rkc_judge(succ_threshold)
+      return rkc(msg, dc)
+   end
+   -- 如果是rkd检定
+   if isrkd then
+      return rkd(msg, dice, dc, at_qq)
    end
    --开始检定
    res = ranint(1, 100)
@@ -433,31 +456,116 @@ Hardness = {
    ["一般"] = NormalJudge
 }
 
--- rkc检定，技能值+1d100和成功阈值比较
-function rkc_judge(succ_threshold)
-   local random = ranint(1, 100)
-   -- 技能值+1d100
-   num = num + random
-   random = string.format("%.0f", random)
-   -- 没有成功阈值
-   if not succ_threshold then
-      return "{pc}进行ark的" ..
-         obj .. "检定:\n" .. obj .. "+1d100(" .. random .. ")=" .. string.format("%.0f", num) .. "，希望你成功吧"
+-- rkc检定，技能值d6+基础属性值和dc比较
+function rkc(msg, dc)
+   local res, random, basic_attr_value, res
+   -- 判断是否是基础属性
+   local basic_attr_input = is_basic_attr(obj)
+   if basic_attr_input then
+      basic_attr_value = getPlayerCardAttr(msg.uid, msg.gid, basic_attr_input, 0)
+      res = ndm(basic_attr_value, 6)
+   else
+      --获取技能对应的基础属性
+      local basic_attr = find_basic_attr(obj)
+      if not basic_attr then
+         return "未找到对应的基础属性，请检查技能名是否正确"
+      end
+      random = ndm(num, 6)
+      basic_attr_value = getPlayerCardAttr(msg.uid, msg.gid, basic_attr, 0)
+      -- 技能值d6+基础属性值
+      res = random + basic_attr_value
    end
-   succ_threshold = tonumber(succ_threshold)
-   if num >= succ_threshold then
+   -- 没有dc
+   if not dc then
+      if basic_attr_input then
+         return "{pc}进行ark的" .. obj .. "检定:\n" .. basic_attr_value .. "d6=" .. res .. "，希望你成功吧"
+      end
+      return "{pc}进行ark的" .. obj .. "检定:\n" .. num .. "d6+" .. basic_attr_value .. "=" .. res .. "，希望你成功吧"
+   end
+   dc = tonumber(dc)
+   if res >= dc then
+      if basic_attr_input then
+         return "{pc}进行ark的" ..
+            obj .. "检定:\n" .. basic_attr_value .. "d6=" .. res .. "/" .. dc .. "，{strRollRegularSuccess}"
+      end
       return "{pc}进行ark的" ..
+         obj .. "检定:\n" .. num .. "d6+" .. basic_attr_value .. "=" .. res .. "/" .. dc .. "，{strRollRegularSuccess}"
+   else
+      if basic_attr_input then
+         return "{pc}进行ark的" .. obj .. "检定:\n" .. basic_attr_value .. "d6=" .. res .. "/" .. dc .. "，{strRollFailure}"
+      end
+      return "{pc}进行ark的" ..
+         obj .. "检定:\n" .. num .. "d6+" .. basic_attr_value .. "=" .. res .. "/" .. dc .. "，{strRollFailure}"
+   end
+end
+
+-- rkd检定，技能值d骰面+基础属性和dc比较，并将at_qq的hp减去res
+function rkd(msg, dice, dc, at_qq)
+   --dice和dc不为空
+   if not dice or not dc then
+      return "请指定骰面和dc值"
+   end
+   --获取技能对应的基础属性
+   local basic_attr = find_basic_attr(obj)
+   if not basic_attr then
+      return "未找到对应的基础属性，请检查技能名是否正确"
+   end
+   local random = ndm(num, dice)
+   local basic_attr_value = getPlayerCardAttr(msg.uid, msg.gid, basic_attr, 0)
+   -- 技能值d骰面+基础属性值
+   local res = random + basic_attr_value
+   dc = tonumber(dc)
+   if res >= dc then
+      local reply =
+         "{pc}进行ark的" ..
          obj ..
             "检定:\n" ..
-               obj ..
-                  "+1d100(" ..
-                     random .. ")=" .. string.format("%.0f", num) .. "/" .. succ_threshold .. "，{strRollRegularSuccess}"
+               num ..
+                  "d" .. dice .. "+" .. basic_attr_value .. "-" .. dc .. "=" .. (res - dc) .. "，{strRollRegularSuccess}"
+      -- at_qq的hp减去res
+      if not at_qq then
+         return reply
+      end
+      local hp = getPlayerCardAttr(at_qq, msg.gid, "hp", 0)
+      hp = hp - (res - dc)
+      if hp <= 0 then
+         hp = 0
+         reply = reply .. "\n警告：目标hp已经归零"
+      else
+         reply = reply .. "\n目标hp已减少" .. (res - dc) .. "点"
+      end
+      setPlayerCardAttr(at_qq, msg.gid, "hp", hp)
+      return reply
    else
       return "{pc}进行ark的" ..
          obj ..
-            "检定:\n" ..
-               obj ..
-                  "+1d100(" ..
-                     random .. ")=" .. string.format("%.0f", num) .. "/" .. succ_threshold .. "，{strRollFailure}"
+            "检定:\n" .. num .. "d" .. dice .. "+" .. basic_attr_value .. "=" .. res .. "/" .. dc .. "，{strRollFailure}"
    end
+end
+
+function ndm(n, m)
+   local res = 0
+   for _ = 1, n do
+      res = res + ranint(1, m)
+   end
+   return math.floor(res)
+end
+
+function find_basic_attr(str)
+   for k, v in pairs(BASIC_ATTR) do
+      if k:find(str) then
+         return v
+      end
+   end
+   return nil
+end
+
+-- 判断是否是基础属性
+function is_basic_attr(str)
+   for _, v in pairs(BASIC_ATTR) do
+      if v == str then
+         return v
+      end
+   end
+   return nil
 end
